32453a8294066e3bc4a2fbb8cd53dff16d537db0
Mani Batra <s4295255@student.uq.edu.au>
2015-03-31 10:22:42 +1000

    hamming encode function added

--- mylib/s4295255_hamming.c
+++ mylib/s4295255_hamming.c
@@ -40,5 +40,35 @@
 
   */
 
-extern void s4295255_hamming_encode(unsigned char hbyte);
+uint16_t s4295255_hamming_encode(unsigned char hbyte){
+
+	uint8_t d0, d1, d2, d3;
+	uint8_t p0 = 0, h0, h1, h2;
+	uint8_t z;
+	uint8_t out;
+	
+	/* extract bits */
+	d0 = !!(hbyte & 0x1);
+	d1 = !!(hbyte & 0x2);
+	d2 = !!(hbyte & 0x4);
+	d3 = !!(hbyte & 0x8);
+	
+	/* calculate hamming parity bits */
+	h0 = d1 ^ d2 ^ d3;
+	h1 = d0 ^ d2 ^ d3;
+	h2 = d0 ^ d1 ^ d3;
+	
+	/* generate out byte without parity bit P0 */
+	out = (h0 << 4) | (h1 << 5) | (h2 << 6) |
+		(d0 << 0) | (d1 << 1) | (d2 << 2) | (d3 << 3);
+
+	/* calculate even parity bit */
+	for (z = 0; z<7; z++)		
+		p0 = p0 ^ !!(out & (1 << z));
+	
+	out |= (p0 << 7);	//Put P0 into most significatn bit.
+
+	return(out)
+
+}
 extern void s4295255_hamming_decode(unsigned short hword);
