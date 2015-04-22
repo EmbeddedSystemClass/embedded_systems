070ae3ad41b50a213d366a92edfa1a3cb26b86b0
Mani Batra <s4295255@student.uq.edu.au>
2015-04-23 05:59:51 +1000

    updated definitions

--- mylib/s4295255_sysmon.h
+++ mylib/s4295255_sysmon.h
@@ -1,36 +1,41 @@
 /**   
  ******************************************************************************   
- * @file    mylib/s4295255_ledbar.h  
+ * @file    mylib/s4295255_sysmon.c    
  * @author  Mani Batra – 42952556   
- * @date    06032015   
- * @brief   LED Light Bar peripheral driver   
- *	     REFERENCE: LEDLightBar_datasheet.pdf   
+ * @date    23042015   
+ * @brief   Driver for System monitor for FreeRTOS  
+ *	      
  *
- *			NOTE: REPLACE sxxxxxx with your student login.
  ******************************************************************************   
  *     EXTERNAL FUNCTIONS
  ******************************************************************************
- * s4295255_ledbar_init() – intialise LED Light BAR
- * s4295255_ledbar_set() – set LED Light BAR value
+ * s4295255_sysmon_init() – intialise the system monitor on pin A3 to A5
  ******************************************************************************   
  *     REVISION HISTORY
  ******************************************************************************
- * 1. 3/3/2015 - Created
- * 2. 10/3/2015 – Added functionality to set function.  
+ * 1. 23/4/2015 - Created
  */
 
-#ifndef S4295255_LEDBAR_H
-#define S4295255_LEDBAR_H
+#ifndef S4295255_SYSMON_H
+#define S4295255_SYSMON_H
 
 /* Includes ------------------------------------------------------------------*/
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
+
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 
 /* Private function prototypes -----------------------------------------------*/
 
-extern void s4295255_ledbar_init(void);
-extern void s4295255_ledbar_set(unsigned short value);
+#define S4295255_LA_CHAN0_CLR()	HAL_GPIO_WritePin(GPIOC, BRD_A3_PIN, 0)
+#define S4295255_LA_CHAN0_SET()	HAL_GPIO_WritePin(GPIOC, BRD_A3_PIN, 1)
+#define S4295255_LA_CHAN1_CLR()	HAL_GPIO_WritePin(GPIOC, BRD_A4_PIN, 0)
+#define S4295255_LA_CHAN1_SET()	HAL_GPIO_WritePin(GPIOC, BRD_A4_PIN, 1)
+#define S4295255_LA_CHAN2_CLR()	HAL_GPIO_WritePin(GPIOC, BRD_A5_PIN, 0)
+#define S4295255_LA_CHAN2_SET()	HAL_GPIO_WritePin(GPIOC, BRD_A5_PIN, 1)
+	
+
+extern void s4295255_sysmon_init(void);
 #endif
 
